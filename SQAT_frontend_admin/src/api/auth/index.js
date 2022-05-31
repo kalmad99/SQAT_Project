4013dfb3aa55013fd62e0384c0812f7de0a5b3a9
kalmad99 <56146523+kalmad99@users.noreply.github.com>
2022-05-31 17:38:56 +0300

    [web-admin-user] Adding logout feature (#15)

--- SQAT_frontend_admin/src/api/auth/index.js
+++ SQAT_frontend_admin/src/api/auth/index.js
@@ -12,24 +12,6 @@ export const login = async (email, password) => {
     }
 }
 
-// export const sendEmail = async (email) => {
-//     try {
-//         const { data: result } = await axios.post("http://localhost:8080/login", { email })
-//         localStorage.setItem("token", result.token)
-//         return result;
-//     } catch (error) {
-//         console.log(error)
-//         throw new Error("Invalid Email or Password")
-//     }
-// }
-
-// export const userLogin = async (email, password) => {
-//     try {
-//         const { data: result } = await axios.post("https://e909-197-156-118-253.eu.ngrok.io/login", { email })
-//         localStorage.setItem("token", result.token)
-//         return result;
-//     } catch (error) {
-//         console.log(error)
-//         throw new Error("Invalid Email or Password")
-//     }
-// }
+export const logout = () => {
+    localStorage.removeItem("token")
+} 
